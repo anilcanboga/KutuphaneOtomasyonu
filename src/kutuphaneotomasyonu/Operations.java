922b6a11e70b33835b5eb95f57eac42e68634c48
anilcanboga <anlcn.boga@hotmail.com>
2021-01-27 00:23:25 +0300

    Operations Güncelleme
    - Add, Update, Delete Data Eklendi.

--- src/kutuphaneotomasyonu/Operations.java
+++ src/kutuphaneotomasyonu/Operations.java
@@ -17,6 +17,50 @@ public class Operations {
     Statement sta = null;
     PreparedStatement psta = null;
 
+    public void bookDelete(int id) {
+
+        String sorgu = "Delete from books_database WHERE id=?";
+        try {
+            psta = con.prepareStatement(sorgu);
+            psta.setInt(1, id);
+            psta.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Operations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void bookUpdate(int id, String new_name, String new_writer, String new_type, String new_publisher) {
+
+        String sorgu = "Update books_database SET book_name=?,book_writer=?,book_type=?,book_publisher=? WHERE id=?";
+        try {
+            psta = con.prepareStatement(sorgu);
+            psta.setString(1, new_name);
+            psta.setString(2, new_writer);
+            psta.setString(3, new_type);
+            psta.setString(4, new_publisher);
+            psta.setInt(5, id);
+            psta.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Operations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void bookAdd(String name, String writer, String type, String publisher) {
+
+        String sorgu = "Insert into books_database(book_name, book_writer, book_type, book_publisher) VALUES (?,?,?,?)";
+        try {
+            psta = con.prepareStatement(sorgu);
+            psta.setString(1, name);
+            psta.setString(2, writer);
+            psta.setString(3, type);
+            psta.setString(4, publisher);
+            psta.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Operations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public ArrayList<Book> BookCome() {
         ArrayList<Book> list = new ArrayList<Book>();
         String sorgu = "Select * from books_database";
@@ -60,7 +104,7 @@ public class Operations {
     public Operations() {
 
         //url -> jdbc:mysql://host:port/db_name;id;password;
-        String url = "jdbc:mysql://" + Database.host + ":" + Database.port + "/" + Database.db_name;
+        String url = "jdbc:mysql://" + Database.host + ":" + Database.port + "/" + Database.db_name + "?useUnicode=true&characterEncoding=UTF-8";
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
